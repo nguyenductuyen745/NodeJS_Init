3c5748f1b2ef0d1e449c8e5472cbe9c38a9726a8
John <nguyenductuyen745@gmail.com>
2025-09-11 22:24:23 +0700

    feat: Add routes structure

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 // Https logging
 app.use(morgan("combined"));
 app.use(express.static(path.join(__dirname, "public")));
@@ -17,12 +19,12 @@ app.engine(
     extname: "hbs",
   })
 );
+
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "/resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+// Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
