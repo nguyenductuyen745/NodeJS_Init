be80648eba038670f44f66f520c49756d566dc4d
John <nguyenductuyen745@gmail.com>
2025-09-17 22:24:23 +0700

    feat: add prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const { engine } = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
 
 const app = express();
 const port = 3000;
 
-const route = require("./routes");
+const route = require('./routes');
 
 // Https logging
-app.use(morgan("combined"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Templates engine
 app.engine(
-  "hbs",
+  'hbs',
   engine({
-    extname: "hbs",
-  })
+    extname: 'hbs',
+  }),
 );
 
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "/resources/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/resources/views'));
 
 // Route init
 route(app);
